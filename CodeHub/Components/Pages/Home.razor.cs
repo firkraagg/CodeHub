e786132cd59195cbfeac2338977872735be13628
firkraagg <danzuz009@yahoo.com>
2025-02-05 19:31:34 +0100

    Sorting problems based on difficulty working

--- CodeHub/Components/Pages/Home.razor.cs
+++ CodeHub/Components/Pages/Home.razor.cs
@@ -5,9 +5,13 @@ namespace CodeHub.Components.Pages;
 public partial class Home
 {
     private List<Problem> _problems = new();
+    private List<Problem> _filteredProblems = new();
+    private int _selectedDifficulty = 0;
     private int _maxProblemsToShow = 20;
     private int _currentPage = 1;
-    private int _totalPages =>  (int)Math.Ceiling((double)_problems.Count / _maxProblemsToShow);
+    private int _totalPages => (int)Math.Ceiling((double)(IsFiltered ? _filteredProblems.Count : _problems.Count) / _maxProblemsToShow);
+    private bool IsFiltered => _selectedDifficulty > 0;
+
 
     protected override async Task OnInitializedAsync()
     {
@@ -29,5 +33,14 @@ public partial class Home
     public async Task LoadProblems()
     {
         _problems = await ProblemService.GetProblemsAsync();
+    }
+
+    private void FilterByDifficulty(int difficulty)
+    {
+        _selectedDifficulty = difficulty;
+        _filteredProblems = difficulty == 0
+            ? _problems
+            : _problems.Where(p => p.Difficulty == difficulty).ToList();
+        _currentPage = 1;
     }
 }
