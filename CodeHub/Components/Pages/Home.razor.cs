6c761867d238ea2c30204f33a96b563bb0cbe807
firkraagg <danzuz009@yahoo.com>
2025-02-05 09:29:38 +0100

    Full working pagination in Home page for Problems

--- CodeHub/Components/Pages/Home.razor.cs
+++ CodeHub/Components/Pages/Home.razor.cs
@@ -6,14 +6,28 @@ public partial class Home
 {
     private List<Problem> _problems = new();
     private int _maxProblemsToShow = 20;
+    private int _currentPage = 1;
+    private int _totalPages =>  (int)Math.Ceiling((double)_problems.Count / _maxProblemsToShow);
 
     protected override async Task OnInitializedAsync()
     {
-        _problems = await ProblemService.GetProblemsAsync();
+        await LoadProblems();
     }
 
     public void SetProblemCount(int count)
     {
         _maxProblemsToShow = count;
+        _currentPage = 1;
+    }
+
+    private void GoToPage(int page)
+    {
+        if (page < 1 || page > _maxProblemsToShow) return;
+        _currentPage = page;
+    }
+
+    public async Task LoadProblems()
+    {
+        _problems = await ProblemService.GetProblemsAsync();
     }
 }
