9c8ef698992f408595227b630d042c70382fbf76
firkraagg <danzuz009@yahoo.com>
2025-02-16 20:38:14 +0100

    Added Monaco Editor

--- CodeHub/Components/Pages/ProblemDetails.razor.cs
+++ CodeHub/Components/Pages/ProblemDetails.razor.cs
@@ -2,6 +2,7 @@
 using CodeHub.Data.Models;
 using CodeHub.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace CodeHub.Components.Pages;
 
@@ -11,10 +12,44 @@ public partial class ProblemDetails
 
     private Problem? _problem;
     private List<Tag> _tags = new();
+    private string _selectedLanguage = "java";
+    private string _selectedTheme = "vs-dark";
 
     protected override async Task OnInitializedAsync()
     {
         _problem = await ProblemService.GetProblemByIdAsync(ProblemId);
         _tags = await TagService.GetTagsForProblemAsync(_problem.Id);
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await JS.InvokeVoidAsync("monacoInterop.initialize", "editorContainer", "csharp");
+        }
+    }
+
+    private async Task GetEditorValue()
+    {
+        string code = await JS.InvokeAsync<string>("monacoInterop.getValue");
+        Console.WriteLine($"Code from editor: {code}");
+    }
+
+    private async Task ChangeTheme(string theme)
+    {
+        _selectedTheme = theme;
+        await JS.InvokeVoidAsync("monacoInterop.setTheme", _selectedTheme);
+    }
+
+    private async Task ChangeLanguage(string language)
+    {
+        _selectedLanguage = language switch
+        {
+            "Java" => "java",
+            "C#" => "csharp",
+            _ => "plaintext"
+        };
+
+        await JS.InvokeVoidAsync("monacoInterop.setLanguage", _selectedLanguage);
     }
 }
---
+++ CodeHub/wwwroot/monacoInterop.js
@@ -0,0 +1,38 @@
+﻿window.monacoInterop = {
+    editorInstance: null,
+
+    initialize: function (elementId, language) {
+        require.config({ paths: { vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.43.0/min/vs' } });
+        require(['vs/editor/editor.main'], function () {
+            window.monacoInterop.editorInstance = monaco.editor.create(document.getElementById(elementId), {
+                language: language,
+                theme: 'vs-dark',
+                automaticLayout: true,
+                quickSuggestions: { other: true, comments: false, strings: true },
+                smoothScrolling: true
+            });
+
+            window.monacoInterop.editorInstance.setValue("//sem napíšte svoj kód");
+        });
+    },
+
+    setTheme: (theme) => {
+        if (window.monacoInterop.editorInstance) {
+            monaco.editor.setTheme(theme);
+        } else {
+            console.error("Monaco Editor instance not found!");
+        }
+    },
+
+    setLanguage: (language) => {
+        if (window.monacoInterop.editorInstance) {
+            monaco.editor.setModelLanguage(window.monacoInterop.editorInstance.getModel(), language);
+        } else {
+            console.error("Monaco Editor instance not found!");
+        }
+    },
+
+    getValue: function () {
+        return window.monacoInterop.editorInstance ? window.monacoInterop.editorInstance.getValue() : "";
+    }
+};
